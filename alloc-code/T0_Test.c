9d9409aa21887bf024d53d875ec53d3e6ad3d18a
adalberto027 <adalberto2772003@gmail>
2025-03-18 13:02:49 -0600

    new

--- alloc-code/T0_Test.c
+++ alloc-code/T0_Test.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/mman.h>
+
+#define PAGE_SIZE 4096  // standard page size (4KB)
 
 int main() {
-    // print the process ID (PID) for tracking
+    // print process ID
     printf("Process running. PID: %d\n", getpid());
+
+    // memory map an anonymous 4KB page
+    void *mapped_mem = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    if (mapped_mem == MAP_FAILED) {
+        perror("mmap failed");
+        exit(1);
+    }
+
+    printf("Memory mapped at address: %p\n", mapped_mem);
     printf("Press Enter to exit...\n");
 
-    // keep the program running until the user presses Enter
+    // wait for a user input
     getchar();
 
+    // clean up the mapped memory
+    munmap(mapped_mem, PAGE_SIZE);
     return 0;
 }
